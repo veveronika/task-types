eb39be1af15875eb9da7be9d4afc6b87a8d70236
veveronika <vera.suleymanova.05@bk.ru>
2025-05-07 19:36:06 +0300

    Задание по HTML

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,21 +1,15 @@
-/*
-Типизируйте функцию getPersons, возвращающую массив объектов со следующими свойствами:
-- name - строка
-- age - число
-- gender - 'male' или 'female'
- */
-export function getPersons();
+type Gender = 'male' | 'female';
 
-/*
-Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
-   1. Пользователь:
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   2. Сотрудник
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   - company- строка
- */
-export function personToString(person);
+interface Person {
+  name: string;
+  age: number;
+  gender: Gender;
+}
+
+interface Employee extends Person {
+  company: string;
+}
+
+export function getPersons(): Person[];
+
+export function personToString(person: Person | Employee): string;
